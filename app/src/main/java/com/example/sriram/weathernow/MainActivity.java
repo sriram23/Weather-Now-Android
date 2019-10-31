a187b928f9206b2749adfba25be72572b32543b6
sriram23 <sriramsubramanian23@gmail.com>
2019-10-31 18:57:48 +0530

    Search options enabled. Translation enabled.

--- app/src/main/java/com/example/sriram/weathernow/MainActivity.java
+++ app/src/main/java/com/example/sriram/weathernow/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.sriram.weathernow;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.drawable.GradientDrawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,20 +35,32 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ImageView flag_country,WeatherImgView;
-    TextView CityTV, Temperature_mainTV,WeatherDescTV,Temp_minTV,Temp_maxTV,WindSpeedTV;
+    TextView CityTV, Temperature_mainTV,WeatherDescTV,Temp_minTV,Temp_maxTV,WindSpeedTV,Sunrise,Sunset,LastUpdate,WindTV;
     LocationManager locationManager;
+    ScrollView scroll;
+    ProgressBar pbar;
     final String KEY = "7727eb7a7ad3adf1d307938860eca01b";
     String latlon_link;
+    String yandex_trans;
+    String lang;
+    final String KEY_YANDEX = "trnsl.1.1.20191019T094233Z.0ba61e7230e4586e.2e50d940f6626b2fb2a0c141226169539b91aeef";
     String Lat;
     String Lon;
+    String Con;
+    String flag_link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
+        Con = "IN";
         Temperature_mainTV = findViewById(R.id.TempMain);
         CityTV = findViewById(R.id.City);
         WeatherDescTV = findViewById(R.id.WeatherDesc);
@@ -45,10 +68,23 @@ public class MainActivity extends AppCompatActivity {
         Temp_maxTV = findViewById(R.id.temp_max);
         WindSpeedTV = findViewById(R.id.WindSpeed);
         WeatherImgView = findViewById(R.id.WeatherImg);
+        Sunrise = findViewById(R.id.sunriseTV);
+        Sunset = findViewById(R.id.sunsetTV);
+        LastUpdate = findViewById(R.id.lastUpdate);
+        WindTV = findViewById(R.id.windTextView);
+        scroll = findViewById(R.id.scrollView);
+        pbar = findViewById(R.id.progress);
+
+        scroll.setVisibility(View.INVISIBLE);
 
         latlon_link = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric&apikey=%s";
-        Lat = "11.1045297";
-        Lon = "76.9378387";
+        yandex_trans = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=%s&text=%s&lang=%s";
+        flag_link = "https://www.countryflags.io/%s/shiny/64.png";
+        lang = "ta";
+//        Lat = "11.1045297";
+//        Lon = "76.9378387";
+        Lat = "11.0165332";
+        Lon = "76.9694461";
 //        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 //        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 //            // TODO: Consider calling
@@ -63,9 +99,8 @@ public class MainActivity extends AppCompatActivity {
 //        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
         String FinalLink = String.format(latlon_link,Lat,Lon,KEY);
         flag_country = findViewById(R.id.country_flag);
-        String flag_link = "https://www.countryflags.io/%s/shiny/64.png";
         CountryAsyncTask task = new CountryAsyncTask();
-        task.execute(flag_link);
+        task.execute(flag_link,Con);
         WeatherAsyncTask task1 = new WeatherAsyncTask();
         task1.execute(FinalLink);
     }
@@ -75,6 +110,48 @@ public class MainActivity extends AppCompatActivity {
         String FinalLatLon_Link = String.format(latlon_link,Lat,Lng,KEY);
         Toast.makeText(this,"Lat:"+Lat+" Lng:"+Lng,Toast.LENGTH_LONG );
     }
+
+    public void getLatLon(View view) {
+        try {
+            EditText et = findViewById(R.id.CitySearch);
+            String location = et.getText().toString();
+            Geocoder gc = new Geocoder(this);
+            List<Address> list = gc.getFromLocationName(location, 1);
+            Log.e("ERROR_MAHN", String.valueOf(list.isEmpty()));
+            if(list.isEmpty()){
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle(getString(R.string.ta_location_err_title));
+                alertDialog.setMessage(getString(R.string.ta_location_err));
+                alertDialog.show();
+                Toast.makeText(MainActivity.this,getString(R.string.ta_location_err),Toast.LENGTH_LONG);
+                Log.e("ERROR_MAHN","Invalid!");
+            }
+            else {
+                Address address = list.get(0);
+                Log.e("ERROR_MAHN","Valid! "+list.toString());
+                Toast.makeText(this,list.toString(),Toast.LENGTH_LONG);
+                String locality = address.getLocality();
+//                Toast.makeText(MainActivity.this,locality,Toast.LENGTH_LONG);
+                Lat = String.valueOf(address.getLatitude());
+                Lon = String.valueOf(address.getLongitude());
+                Log.e("ERROR_MAHN",Lat+" "+Lon+" "+locality);
+                String FinalLink = String.format(latlon_link, Lat, Lon, KEY);
+                WeatherAsyncTask task = new WeatherAsyncTask();
+                task.execute(FinalLink);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("ERROR_MAHN",e.toString());
+        }
+    }
+
+    public void gotoYandex(View view) {
+//        startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse("translate.yandex.com")));
+        WebView webView = findViewById(R.id.webV);
+        webView.loadUrl("translate.yandex.com");
+    }
+
+
     protected class CountryAsyncTask extends AsyncTask<String, Void, Bitmap> {
 
         @Override
@@ -82,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
 
             Bitmap mIcon_val = null;
             try {
-                URL imgURL = new URL(String.format(link[0], "IN"));
+                URL imgURL = new URL(String.format(link[0], Con));
                 mIcon_val = BitmapFactory.decodeStream(imgURL.openConnection().getInputStream());
 //                Thread.sleep(5000);
 //
@@ -106,19 +183,26 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String[] doInBackground(String... voids) {
             try {
+//                scroll.setVisibility(View.GONE);
+//                pbar.setVisibility(View.VISIBLE);
                 URL weatherURL = new URL(voids[0]);
                 BufferedReader br = new BufferedReader(new InputStreamReader(weatherURL.openConnection().getInputStream(),"UTF-8"));
                 String JSONBase =  br.readLine();
                 JSONObject base = new JSONObject(JSONBase);
-                result[0] = base.getString("name");
+                result[0] = translate_ta(base.getString("name"));
                 result[1] = String.valueOf(base.getJSONObject("main").getDouble("temp"));
                 result[2] = base.getJSONArray("weather").getJSONObject(0).getString("main");
-                result[3] = base.getJSONArray("weather").getJSONObject(0).getString("description");
+                result[3] = translate_ta("Weather: "+base.getJSONArray("weather").getJSONObject(0).getString("description"));
                 result[4] = String.valueOf(base.getJSONObject("main").getDouble("temp_min"));
                 result[5] = String.valueOf(base.getJSONObject("main").getDouble("temp_max"));
                 result[6] = base.getJSONObject("wind").getString("speed");
                 result[7] = base.getJSONObject("wind").getString("deg");
                 result[8] = String.valueOf(getClimate(result[2]));
+                result[9] = convertTime(base.getJSONObject("sys").getLong("sunrise"));
+                result[10] = convertTime(base.getJSONObject("sys").getLong("sunset"));
+                result[11] = getString(R.string.ta_lastupdate)+convertTime(base.getLong("dt"));
+                result[12] = getString(R.string.ta_wind);
+                result[13] = base.getJSONObject("sys").getString("country");
 
 //                String CityName = base.getString("name");
 //                String Temperature = String.valueOf(base.getJSONObject("main").getDouble("temp"));
@@ -136,9 +220,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            scroll.setVisibility(View.GONE);
+            pbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected void onPostExecute(String[] aVoid) {
             super.onPostExecute(aVoid);
-            Toast.makeText(MainActivity.this, aVoid[0]+" "+aVoid[1], Toast.LENGTH_SHORT).show();
+//            Toast.makeText(MainActivity.this, aVoid[0]+" "+aVoid[1], Toast.LENGTH_SHORT).show();
             Temperature_mainTV.setText(aVoid[1]+"°C");
             GradientDrawable tempCircle = (GradientDrawable) Temperature_mainTV.getBackground();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
@@ -148,12 +239,26 @@ public class MainActivity extends AppCompatActivity {
 //                tempCircle.setColor(color);
             }
             CityTV.setText(aVoid[0]);
-            WeatherDescTV.setText("Weather: "+aVoid[3]);
+            WeatherDescTV.setText(aVoid[3]);
             Temp_minTV.setText(aVoid[4]+"°C");
             Temp_maxTV.setText(aVoid[5]+"°C");
             WindSpeedTV.setText(aVoid[6]+" m/s");
             WeatherImgView.setImageResource(Integer.parseInt(aVoid[8]));
 //            roateImage(WeatherImgView,Integer.parseInt(aVoid[7]));
+//            Toast.makeText(MainActivity.this, aVoid[9]+" "+aVoid[10], Toast.LENGTH_SHORT).show();
+            Sunrise.setText(aVoid[9]);
+            Sunset.setText(aVoid[10]);
+            LastUpdate.setText(aVoid[11]);
+            WindTV.setText(aVoid[12]);
+            scroll.setVisibility(View.VISIBLE);
+            pbar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this,aVoid[13],Toast.LENGTH_LONG);
+            Con = aVoid[13];
+            String flag_link = "https://www.countryflags.io/%s/shiny/64.png";
+            Toast.makeText(MainActivity.this,"Flag Task Starting",Toast.LENGTH_LONG);
+            CountryAsyncTask task = new CountryAsyncTask();
+            task.execute(flag_link,aVoid[13]);
+
         }
 
         int getTempColor(String magnitude){
@@ -199,5 +304,32 @@ public class MainActivity extends AppCompatActivity {
 //            matrix.postRotate((float) deg, imageView.getDrawable().getBounds().width()/2,    imageView.getDrawable().getBounds().height()/2);
 //            imageView.setImageMatrix(matrix);
 //        }
+
+        String convertTime(long epoch){
+            Date date = new Date(epoch*1000);
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:MM a z");
+            String human = sdf.format(date);
+            return human;
+        }
+
+        private String translate_ta(String name) {
+            String resp = null;
+            try {
+                URL transURL = new URL(String.format(yandex_trans, KEY_YANDEX, name, lang));
+                BufferedReader br = new BufferedReader(new InputStreamReader(transURL.openConnection().getInputStream(), "UTF-8"));
+                String Jresp = br.readLine();
+                JSONObject base = new JSONObject(Jresp);
+                resp = base.getJSONArray("text").getString(0);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return resp;
+        }
     }
 }
